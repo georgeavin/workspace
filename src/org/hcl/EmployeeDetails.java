4c8e0b962c52b84dec324b6afee025cefd02abe2
georgeavin <jgeorgevavinash@gmail.com>
2022-03-13 14:41:10 +0530

    New Class file was Added

---
+++ src/org/hcl/EmployeeDetails.java
@@ -0,0 +1,51 @@
+//create the Package Name using only small letter and seprated by the dot
+package org.hcl;
+
+//create a class name
+public class EmployeeDetails {
+	
+//create a methods inside the class
+	
+	
+	//method1
+	
+	private void employeeName() {
+		
+		System.out.println("Employee Name : Ram");
+		
+	}
+	//method2
+		
+	private void employeeId() {
+		
+		System.out.println("Employee Id : 2888");
+	
+		
+	}
+	
+	//Main method creation
+	//main ctrl+space, enter
+	
+	public static void main(String[] args) {
+		
+		
+		//object creation
+		//Formula : ClassName objectName=new ClassName();
+		
+		EmployeeDetails e=new EmployeeDetails();
+		
+		//Method Calling
+		//Formula : objectName.methodName();
+		
+		e.employeeId();
+		e.employeeName();
+	}
+		
+	
+}
+	
+	
+
+
+
+
